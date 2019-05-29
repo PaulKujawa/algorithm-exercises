0332a6bb6dd2e92f1fa239ce0a7ee6634bfd207e
Paul Kujawa <p.kujawa@gmx.net>
2019-05-30 00:22:59 +0200

    Update selection-sort.js

--- sorting/selection-sort.js
+++ sorting/selection-sort.js
@@ -1,15 +1,15 @@
 // O(N^2)
 function selectionSort(list) {
-  const indexOfMaxNumber = () => arr.reduce(
-    (acc, elem, idx) => elem > acc.max ? {max: elem, idx} : acc,
-    {max: arr[0], idx: 0},
+  const indexOfMaxNumber = (arr) => arr.reduce(
+    (max, elem, idx) => elem > max.elem ? {elem, idx} : max,
+    {elem: arr[0], idx: 0},
   );
 
   const arr = [...list];
   const sorted = [];
 
   while (arr.length) {
-    const {idx} = indexOfMaxNumber();
+    const {idx} = indexOfMaxNumber(arr);
     sorted.unshift( ...arr.splice(idx, 1) );
   }
 	
