9cb3b0ff9f4094e59d725f18b3e35112a164c207
Paul Kujawa <p.kujawa@gmx.net>
2019-05-30 00:20:45 +0200

    Update selection-sort.js

--- sorting/selection-sort.js
+++ sorting/selection-sort.js
@@ -1,20 +1,19 @@
 // O(N^2)
-function selectionSort(arr) {
-	const indexOfMaxNumber = () => arr.reduce(
+function selectionSort(list) {
+  const indexOfMaxNumber = () => arr.reduce(
     (acc, elem, idx) => elem > acc.max ? {max: elem, idx} : acc,
     {max: arr[0], idx: 0},
   );
 
+  const arr = [...list];
+  const sorted = [];
 
-	const arr = [...list];
-	const sorted = [];
-
-	while (arr.length) {
-		const {idx} = indexOfMaxNumber();
-		sorted.unshift( ...arr.splice(idx, 1) );
+  while (arr.length) {
+    const {idx} = indexOfMaxNumber();
+    sorted.unshift( ...arr.splice(idx, 1) );
   }
 	
-	return sorted;
+  return sorted;
 }
 
 module.exports = selectionSort;
