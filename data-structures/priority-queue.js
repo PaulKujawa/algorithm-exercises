04a1d573894365846185c8c648139866a958d5e8
Paul Kujawa <p.kujawa@gmx.net>
2019-06-01 17:48:45 +0200

    Update priority-queue.js

--- data-structures/priority-queue.js
+++ data-structures/priority-queue.js
@@ -1,24 +1,24 @@
 function createPriorityQueue() {
   const prioItems = [];
-	const items = [];
+  const items = [];
 
-	return {
-		enqueue(item, prio = false) {
-			prio ? prioItems.push(item) : items.push(item);
-		},
-		dequeue() {
-			return prioItems.length ? prioItems.shift() : items.shift();
-		},
-		get length() {
-			return items.length + prioItems.length;
-		},
-		peek() {
-			return prioItems.length ? prioItems[0] : items[0];
-		},
-		isEmpty() {
-			return prioItems.length === 0 && items.length === 0;
-		}
-	}
+  return {
+    enqueue(item, prio = false) {
+      prio ? prioItems.push(item) : items.push(item);
+    },
+    dequeue() {
+      return prioItems.length ? prioItems.shift() : items.shift();
+    },
+    get length() {
+      return items.length + prioItems.length;
+    },
+    peek() {
+      return prioItems.length ? prioItems[0] : items[0];
+    },
+    isEmpty() {
+      return prioItems.length === 0 && items.length === 0;
+    }
+  }
 }
 
 module.exports = createPriorityQueue;
